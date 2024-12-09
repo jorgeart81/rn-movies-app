45aef2ef45c8262fe11c3caa4039871855591ee3
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-12-09 18:28:58 -0500

    add movie.mapper.ts

--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,11 +1,12 @@
 import { movieApi } from '@/core/api/movie-api';
-import { MDBPNowPlayingResponse } from '@/infrastructure/interfaces';
+import type { MDBPNowPlayingResponse, Movie } from '@/infrastructure/interfaces';
+import { MovieMapper } from '@/infrastructure/mappers';
 
-export const nowPlayingAction = async () => {
+export const nowPlayingAction = async (): Promise<Movie[]> => {
   try {
     const { data } = await movieApi.get<MDBPNowPlayingResponse>('/now_playing');
-    console.log(JSON.stringify(data, null, 2));
-    return [];
+    const movies = [...data.results].map(MovieMapper.fromTheMovieDBToMovie);
+    return movies;
   } catch (error) {
     console.log(error);
     throw 'Cannot load now playing movies';
---
+++ infrastructure/mappers/movie.mapper.ts
@@ -0,0 +1,15 @@
+import type { Movie, Result } from '../interfaces';
+
+export class MovieMapper {
+  static fromTheMovieDBToMovie = (movie: Result): Movie => {
+    return {
+      id: movie.id,
+      title: movie.title,
+      description: movie.overview,
+      releaseDate: new Date(movie.release_date),
+      rating: movie.vote_average,
+      poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      backdrop: movie.backdrop_path,
+    };
+  };
+}
