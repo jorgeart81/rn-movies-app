21a44963eabcb107ac2b55c856ef675ebd5034dc
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-12-10 12:39:50 -0500

    add isLoading

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 
 import { useMovies } from '@/presentation/hooks';
 
 const HomeScreen = () => {
   const { nowPlayingQuery } = useMovies();
+
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className='flex-1 justify-center items-center'>
+        <ActivityIndicator size={30} />
+      </View>
+    );
+  }
 
   return (
     <View>
