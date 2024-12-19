b3657ab3610f5e8af13d201c64576ef7f105e2f7
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-12-19 13:24:47 -0500

    get popular movies

---
+++ core/actions/movies/popular.action.ts
@@ -0,0 +1,14 @@
+import { movieApi } from '@/core/api/movie-api';
+import type { MDBNowPlayingResponse, Movie } from '@/infrastructure/interfaces';
+import { MovieMapper } from '@/infrastructure/mappers';
+
+export const popularAction = async (): Promise<Movie[]> => {
+  try {
+    const { data } = await movieApi.get<MDBNowPlayingResponse>('/popular');
+    const movies = [...data.results].map(MovieMapper.fromTheMovieDBToMovie);
+    return movies;
+  } catch (error) {
+    console.log(error);
+    throw 'Cannot load now playing movies';
+  }
+};
