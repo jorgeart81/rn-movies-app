0aa03c05225dbdfe90a009372a8ad3e9694ec090
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-12-19 15:37:21 -0500

    add MovieHorizontalList onScroll

--- presentation/components/movies/MovieHorizontalList.tsx
+++ presentation/components/movies/MovieHorizontalList.tsx
@@ -1,4 +1,11 @@
-import { View, Text, FlatList } from 'react-native';
+import { useRef } from 'react';
+import {
+  FlatList,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Text,
+  View,
+} from 'react-native';
 
 import type { Movie } from '@/infrastructure/interfaces';
 import MoviePoster from './MoviePoster';
@@ -6,9 +13,35 @@ import MoviePoster from './MoviePoster';
 interface Props {
   movies: Movie[];
   title?: string;
+
+  loadNextPage?: () => void;
 }
 
-const MovieHorizontalList = ({ movies, title }: Props) => {
+const MovieHorizontalList = ({ movies, title, loadNextPage }: Props) => {
+  const isLoading = useRef(false);
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (isLoading.current) return;
+
+    const { contentOffset, contentSize, layoutMeasurement } = event.nativeEvent;
+
+    /**
+     * The calculation adds the current horizontal scroll position (contentOffset.x)
+     * to the width of the visible area (layoutMeasurement.width) and adds a 600 pixel offset.
+     * If this sum is greater than or equal to the total width of the content (contentSize.width),
+     * it means that the end of the list has been reached or is close to being reached.
+     */
+    const isEndReached =
+      contentOffset.x + layoutMeasurement.width + 600 >= contentSize.width;
+
+    if (!isEndReached) return;
+
+    isLoading.current = true;
+
+    console.log('load the folowing movies');
+    loadNextPage?.();
+  };
+
   return (
     <View>
       {title && <Text className='text-3xl font-bold px-4 mb-2'>{title}</Text>}
@@ -21,6 +54,7 @@ const MovieHorizontalList = ({ movies, title }: Props) => {
         renderItem={({ item }) => (
           <MoviePoster id={item.id} poster={item.poster} smallPoster />
         )}
+        onScroll={onScroll}
       />
     </View>
   );
