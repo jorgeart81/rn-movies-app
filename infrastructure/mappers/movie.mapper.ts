79abeb00fdc1cd536dacf02b69b8f0f23d428416
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-12-21 08:16:56 -0500

    add fromTheMDBDetailsToCompleteMovie mapper

--- infrastructure/mappers/movie.mapper.ts
+++ infrastructure/mappers/movie.mapper.ts
@@ -1,7 +1,12 @@
-import type { Movie, Result } from '../interfaces';
+import type {
+  CompleteMovie,
+  MDBMovieDetailsResponse,
+  Movie,
+  Result,
+} from '../interfaces';
 
 export class MovieMapper {
-  static fromTheMovieDBToMovie = (movie: Result): Movie => {
+  static fromTheMDBToMovie = (movie: Result): Movie => {
     return {
       id: movie.id,
       title: movie.title,
@@ -10,6 +15,25 @@ export class MovieMapper {
       rating: movie.vote_average,
       poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
       backdrop: movie.backdrop_path,
+    };
+  };
+
+  static fromTheMDBDetailsToCompleteMovie = (
+    movie: MDBMovieDetailsResponse
+  ): CompleteMovie => {
+    return {
+      id: movie.id,
+      title: movie.title,
+      description: movie.overview,
+      releaseDate: new Date(movie.release_date),
+      rating: movie.vote_average,
+      poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+      genres: movie.genres.map((g) => g.name),
+      duration: movie.runtime,
+      budget: movie.budget,
+      originalTitle: movie.original_title,
+      productionCompanies: movie.production_companies.map((c) => c.name),
     };
   };
 }
